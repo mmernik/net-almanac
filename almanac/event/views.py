032c78d249561f24a730493f0c4def83b8cab920
awang.esnet <awang.esnet@b1711e1c-4bb8-11de-a6a3-df26d2c71dbe>
2009-06-01 17:31:29 +0000

    Added more fields to the event model, and updated default data

--- almanac/event/views.py
+++ almanac/event/views.py
@@ -2,15 +2,14 @@ from django.http import Http404
 from django.template import Context, loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-
 from django.shortcuts import render_to_response, get_object_or_404
+
+from almanac.event.models import Event
 
 import logging
 
 def index(request):
     
     logging.info('index hit')
-    h = render_to_response('event/index.html',{'a':'a'})
-    
-    logging.info(h)
+    h = render_to_response('event/index.html',{'events':Event.objects.all()})
     return h
