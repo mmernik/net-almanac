e6e523361f242097b005a3b62450aee6c22ee96c
awang.esnet <awang.esnet@b1711e1c-4bb8-11de-a6a3-df26d2c71dbe>
2009-06-01 19:02:51 +0000

    Added some more basic test cases

--- trunk/almanac/event/tests.py
+++ trunk/almanac/event/tests.py
@@ -1,15 +1,40 @@
 import unittest,datetime,logging
 from event.models import Event
 
-class EventTestCase(unittest.TestCase):
+experiment = "experiment"
+begin_date = datetime.datetime(2008,1,1)
+
+class EventTestCaseSetup(unittest.TestCase):
     def setUp(self):
-        self.exp = Event(name="experiment",
+        #Create 2 objects and save them in the test database.
+        exp = Event(name=experiment,
                          description="a physics experiment", 
-                         begin_time=datetime.date(2008,1,1),
-                         end_time=datetime.date(2008,1,10))
-        self.exp.save()
+                         begin_time=begin_date,
+                         end_time=datetime.datetime(2008,1,10))
+        exp.save()
+        
+        upgrade = Event(name="upgrade",
+                         description="upgrading infrastructure", 
+                         begin_time=datetime.datetime(2008,2,5),
+                         end_time=datetime.datetime(2008,2,12))
+        upgrade.save()
         
     def runTest(self):
-        print "sanity test Running"
+        #Sanity test
         self.assertTrue(True)
+
+class LoggingTestCase(EventTestCaseSetup):
+    def runTest(self):
+        print "There should be 4 logging messages after this message"
+        logging.debug('this is a debug message')
+        logging.info('this is an info message')
+        logging.warn('this is a warning message')
+        logging.error('this is an error message')
+        
+class DatabaseTestCase(EventTestCaseSetup):
+    def runTest(self):
+        print "Objects in database: " + str(Event.objects.all())
+        self.assertTrue(len(Event.objects.all())==2)
+        self.assertTrue(Event.objects.all()[0].name==experiment)
+        self.assertTrue(Event.objects.all()[0].begin_time==begin_date)
         
