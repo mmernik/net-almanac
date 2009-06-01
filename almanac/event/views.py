ee222752ac1d976b0df618b5919bf69809dbbfca
awang.esnet <awang.esnet@b1711e1c-4bb8-11de-a6a3-df26d2c71dbe>
2009-06-01 23:33:26 +0000

    Some housekeeping.  Update some data, README.

--- almanac/event/views.py
+++ almanac/event/views.py
@@ -4,12 +4,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404
 
-from almanac.event.models import Event
+from almanac.event.models import *
 
 import logging
 
 def index(request):
     
-    logging.info('index hit')
-    h = render_to_response('event/index.html',{'events':Event.objects.all()})
+    logging.debug('index hit')
+    h = render_to_response('event/index.html',
+                           {'events':Event.objects.all(),
+                            'tags':Tag.objects.all(),
+                            'ta':TagAssignment.objects.all()})
     return h
