7a8d995ef4af787f8d4ef7a84f119ce44a2dda5f
awang.esnet <awang.esnet@b1711e1c-4bb8-11de-a6a3-df26d2c71dbe>
2009-06-01 17:31:29 +0000

    Added more fields to the event model, and updated default data

--- trunk/almanac/event/models.py
+++ trunk/almanac/event/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     name= models.CharField(max_length=200)
+    
+    description = models.CharField(max_length=200)
+    begin_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    
     def __unicode__(self):
         return self.name
