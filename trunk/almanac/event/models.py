39addd448dfef1e8b6c7fd1a99ad55b63060f4c2
awang.esnet <awang.esnet@b1711e1c-4bb8-11de-a6a3-df26d2c71dbe>
2009-06-01 22:17:01 +0000

    Implement tags as two models.  Added utilities to query tags.  Finally, created all of the data structure.

--- trunk/almanac/event/models.py
+++ trunk/almanac/event/models.py
@@ -1,12 +1,30 @@
 from django.db import models
 
+MAX_LENGTH=200
+
 # Create your models here.
 class Event(models.Model):
-    name= models.CharField(max_length=200)
-    
-    description = models.CharField(max_length=200)
+    name= models.CharField(max_length=MAX_LENGTH)
+    description = models.CharField(max_length=MAX_LENGTH)
     begin_time = models.DateTimeField()
     end_time = models.DateTimeField()
     
+    url = models.CharField(max_length=MAX_LENGTH)
+    router = models.CharField(max_length=MAX_LENGTH)
+    iface = models.CharField(max_length=MAX_LENGTH)
+    
     def __unicode__(self):
         return self.name
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH)
+    def __unicode__(self):
+        return self.name
+
+class TagAssignment(models.Model):
+    event = models.ForeignKey(Event)
+    tag = models.ForeignKey(Tag)
+    def __unicode__(self):
+#        print self
+        return str(self.event) + ", " +str(self.tag)
