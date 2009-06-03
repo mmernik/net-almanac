f526097af9cec4982fe01cf040b185ac38098b0a
awang.esnet <awang.esnet@b1711e1c-4bb8-11de-a6a3-df26d2c71dbe>
2009-06-03 23:51:49 +0000

    Implement an link together a tag detail page.  Use the tag name instead of id to refer to it, going through a non-generic view.

--- trunk/almanac/event/views.py
+++ trunk/almanac/event/views.py
@@ -5,5 +5,15 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404
 
 from almanac.event.models import *
+from tagging.models import *
 
 import logging
+
+def tag(request,tag_name):
+    logging.debug('tag hit')
+    tag = Tag.objects.get(name=tag_name)
+    events = TaggedItem.objects.get_by_model(Event,tag)
+    
+    return render_to_response('event/tag_detail.html',
+                              {'tag':tag,
+                               'events':events})
