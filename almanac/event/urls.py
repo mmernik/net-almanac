c33628e914030b88e3f4da6a4ced6281bede6149
awang.esnet <awang.esnet@b1711e1c-4bb8-11de-a6a3-df26d2c71dbe>
2009-06-03 23:51:49 +0000

    Implement an link together a tag detail page.  Use the tag name instead of id to refer to it, going through a non-generic view.

--- almanac/event/urls.py
+++ almanac/event/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls.defaults import *
 from event.models import *
+from tagging.models import *
 
 info_dict = {
     'queryset': Event.objects.all(),
+}
+
+tags_dict = {
+    'queryset':Tag.objects.all()
 }
 
 
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.list_detail.object_list',info_dict),
     (r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
+    (r'^tag/(?P<tag_name>\w+)/$', 'almanac.event.views.tag'),
+    
 )
