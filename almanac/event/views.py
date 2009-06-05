355ca978c1b39610be30816b0b67631d7f4da3e4
awang.esnet <awang.esnet@b1711e1c-4bb8-11de-a6a3-df26d2c71dbe>
2009-06-05 22:00:37 +0000

    Some initial error checking for create_event view

--- almanac/event/views.py
+++ almanac/event/views.py
@@ -33,39 +33,65 @@ def create_event(request):
     
     logging.info('request.method='+request.method)
     
+    form = EventForm()
+    
+    events = Event.objects.all()
     if request.method == 'GET':
 
-        form = EventForm()
+        
         return render_to_response('event/event_form.html',
                                   {'form':form,
                                    'form_table':form.as_table()})       
 
         
     elif request.method == 'POST':
-        events = Event.objects.all()
         
-        logging.debug(dir(request))
+        try:
+            post_data = request._get_post()
+            validate_post(post_data)
+            
+            logging.debug(dir(request))
+            logging.debug(request._get_post()['name'])
+            
+            
+            new_event = Event(name=post_data['name'],
+                              description=post_data['description'],
+                              #TODO: get these later, maybe use Django input widgets
+                              #begin_time=post_data['begin_time'],
+                              #end_time=post_data['end_time'],
+                              
+                              begin_time=datetime.datetime(2008,1,1),
+                              end_time=datetime.datetime(2008,1,2),
+                              url=post_data['url'],
+                              router=post_data['router'],
+                              iface=post_data['iface'])
         
-        logging.debug(request._get_post()['name'])
+            new_event.save()
         
-        post_data = request._get_post()
+            return render_to_response('event/event_list.html',
+                                      {'object_list':events})
+        except ValueError, e:
+            
+            logging.info('view create_event: bad user input')
+            return render_to_response('event/event_form.html',
+                                      {'form':form,
+                                       'form_table':form.as_table(),
+                                       'error':e.message})
         
-        new_event = Event(name=post_data['name'],
-                          description=post_data['description'],
-                          #TODO: get these later, maybe use Django input widgets
-                          #begin_time=post_data['begin_time'],
-                          #end_time=post_data['end_time'],
-                          
-                          begin_time=datetime.datetime(2008,1,1),
-                          end_time=datetime.datetime(2008,1,2),
-                          url=post_data['url'],
-                          router=post_data['router'],
-                          iface=post_data['iface'])
-        
-        new_event.save()
-        
-        return render_to_response('event/event_list.html',
-                                  {'object_list':events})
+        except Exception, e:
+            
+            error_message = ('exception occurred in view create_event: ' +
+                             str(type(e)) + ': ' +
+                             e.message)
+            
+            logging.info('exception occurred in view create_event: ' + error_message)
+            return render_to_response('event/event_form.html',
+                                      {'form':form,
+                                       'form_table':form.as_table(),
+                                       'error':error_message})
+        else:
+            logging.error('unexpected error occurred during view create_event')
+            
         
 def update_event(request,object_id):
     logging.info('view update_event hit')
@@ -73,7 +99,7 @@ def update_event(request,object_id):
     logging.info('request.method='+request.method)
     
     event = Event.objects.get(id=object_id)
-    
+    events = Event.objects.all()
     if request.method == 'GET':
 
         form = EventForm(instance=event)
@@ -87,3 +113,22 @@ def update_event(request,object_id):
         logging.debug(request._get_post()['name'])
         return render_to_response('event/event_list.html',
                                   {'object_list':events})
+        
+def validate_post(post_data):
+    if post_data['name']=='':
+        raise ValueError('name cannot be empty')
+    if post_data['description']=='':
+        raise ValueError('description cannot be empty')
+    if post_data['url']=='':
+        raise ValueError('url cannot be empty')
+    if post_data['iface']=='':
+        raise ValueError('iface cannot be empty')
+    if post_data['router']=='':
+        raise ValueError('router cannot be empty')
+    
+    
+    
+    
+    
+    
+    
