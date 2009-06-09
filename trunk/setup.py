7e629f3b34a950360906cc2d4316b0a6d82f606a
jdugan.esnet <jdugan.esnet@b1711e1c-4bb8-11de-a6a3-df26d2c71dbe>
2009-06-09 07:28:44 +0000

    add buildout support

---
+++ trunk/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='almanac',
+      version='2009',
+      description='net-almanac provides a simple RESTish API for tracking time related events',
+      author='Andy Wang',
+      author_email='awang@es.net',
+      url='http://code.google.com/p/net-almanac/',
+      packages=['almanac'],
+     )
+
