0c1bca971181262531b44f2cdb3084de6863a410
awang.esnet <awang.esnet@b1711e1c-4bb8-11de-a6a3-df26d2c71dbe>
2009-06-23 21:54:22 +0000

    add a simple footer and an "about" page

--- trunk/almanac/net_almanac/urls.py
+++ trunk/almanac/net_almanac/urls.py
@@ -42,15 +42,8 @@ urlpatterns = patterns('',
          
     (r'^event/filter/$',
       'almanac.net_almanac.views.filter'),
-      
-    #date-based views
-    (r'^event/date/$',
-     'almanac.net_almanac.views.view_by_date'),
     
-    (r'^event/date/(?P<year>\d{4})/$',
-     'almanac.net_almanac.views.view_by_year'),
-     
-    (r'^event/date/(?P<year>\d{4})/(?P<month>\d{2})/$',
-     'almanac.net_almanac.views.view_by_month'),
-    
+    (r'^about/$',
+      'django.views.generic.simple.direct_to_template', 
+      {'template': 'net_almanac/about.html'}),
 )
