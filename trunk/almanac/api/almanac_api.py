da4a5d3383f129b32945aee1dad183bcb392f3d4
awang.esnet <awang.esnet@b1711e1c-4bb8-11de-a6a3-df26d2c71dbe>
2009-07-03 01:26:35 +0000

    Mostly finish the API.  Implemented getting, filtering, creating, and editing events.  Tests in the main code directory.

--- trunk/almanac/api/almanac_api.py
+++ trunk/almanac/api/almanac_api.py
@@ -5,6 +5,7 @@ import dateutil.parser
 
 """
 A python interface to almanac's REST requests.
+This shouldn't refer to any files in the main branch.
 """
 
 
@@ -15,12 +16,96 @@ JSON_HEADERS = {'accept':'application/json'}
 MAX_LENGTH_FIELD = 100
 MAX_LENGTH_DESCRIPTION = 500
 
+class NetAlmanac():
+    """
+    given the root URL of a net_almanac server, such as 
+    
+    http://www.example.com/net_almanac/event/
+    """
+    def __init__(self, url):
+        self.url = url
+
+    def get_all_events(self):
+        """
+        given the root URL of a net_almanac server, such as 
+        
+        http://www.example.com/net_almanac/event/
+        
+        returns a list of Events
+        """
+        http = httplib2.Http()
+        response, content = http.request(self.url, 'GET', headers = JSON_HEADERS)
+        return deserialize_events(content)
+    
+    def get_event(self, event_id):
+        """
+        returns one Event with the specific id.  Will return None if there is no Event.
+        """
+        http = httplib2.Http()
+        response, content = http.request(self.url + str(event_id) + '/', 'GET', headers = JSON_HEADERS)
+        if response.status == 404:
+            return None
+        return deserialize_events(content)[0]
+    
+    def get_filtered_events(self,
+                            search=None,
+                            name=None,
+                            description=None,
+                            tags=None,
+                            date=None,
+                            begin_date=None,
+                            end_date=None):
+        http = httplib2.Http()
+        
+        filters = []
+        if search:
+            filters.append("search=" + search)
+        if name:
+            filters.append("name=" + name)
+        if description:
+            filters.append("description=" + description)
+        if date:
+            filters.append("date=" + date.strftime('%Y-%M-%d'))
+        if begin_date:
+            filters.append("begin_date=" + begin_date.strftime('%Y-%M-%d'))
+        if end_date:
+            filters.append("end_date=" + end_date.strftime('%Y-%M-%d'))
+        
+        if tags:
+            for tag in tags:
+                filters.append("tag=" + tag)
+        
+        url = self.url + "?" + "&".join(filters)
+        
+        response, content = http.request(url, 'GET', headers = JSON_HEADERS)
+        return deserialize_events(content)
+    
+    def save_event(self,
+                   event):
+        http = httplib2.Http()
+       
+        input = serialize_events([event])
+        response, content = http.request(self.url + str(event.id) + '/', 'PUT', input, headers = JSON_HEADERS)
+        
+        if response.status != HTTP_OK:
+            raise ValueError('Event not saved correctly. Message from server: ' + content)
+        
+    def create_event(self,
+                     event):
+        http = httplib2.Http()
+       
+        input = serialize_events([event])
+        response, content = http.request(self.url , 'POST', input, headers = JSON_HEADERS)
+        
+        if response.status != HTTP_OK:
+            raise ValueError('Event not created correctly. Message from server: ' + content)
+        
 
 class Event():
-    
     """
     An event represents one event object in the almanac database.
-    See README.txt for more information.
+    See README.txt for more information.  This is a local object, we need to
+    call functions save_event or create_event to modify the database with it.
     """
     
     def __init__(self,
@@ -44,12 +129,9 @@ class Event():
         self.router = router #any string
         self.tags = tags #any string
     
-    def __unicode__(self):
-        return self.name + ", id: " + self.id
+    def __str__(self):
+        return self.name + ", id: " + str(self.id)
     
-    def save(self):
-        #to implement
-        pass
 
 def validate_event(event):
     """
@@ -58,24 +140,25 @@ def validate_event(event):
     raises ValueError when there is some inconsistency
     """
     
-    if (event.__class__ != Event):
-        raise ValueError('Object is not of type Event')
-    
     if is_empty_or_space(event.name):
         raise ValueError("'name' property cannot be empty.")
     if is_empty_or_space(event.description):
         raise ValueError("'description' property cannot be empty.")
     
-    for tag in tagging.utils.parse_tag_input(event.tags):
-        #normally commas are delimiters, but if they are between double-quotes they become tags
-        if not tag.isalnum():
-            raise ValueError('Tags must be alphanumeric [a-z][A-Z][0-9].')
-    event.tags = format_tag_string(event.tags)
+    if (len(event.name) > MAX_LENGTH_FIELD
+        or len(event.url) > MAX_LENGTH_FIELD
+        or len(event.router) > MAX_LENGTH_FIELD
+        or len(event.iface) > MAX_LENGTH_FIELD
+        or len(event.description) > MAX_LENGTH_DESCRIPTION):
+        raise ValueError("Field data too long")
+    
+    if not is_alnum_or_space(event.tags):
+        raise ValueError('Tags must be alphanumeric [a-z][A-Z][0-9].')
+    
     if event.end_datetime < event.begin_datetime:
         raise ValueError('The end date cannot be before the begin date.')
-
-def is_empty_or_space(input_string):
-    return (input_string == None or input_string == '' or input_string.isspace())
+    
+    return True    
 
 def serialize_events(events):
     """
@@ -85,7 +168,7 @@ def serialize_events(events):
     for event in events:
         json_event = {}
         json_event['pk'] = event.id
-        json_event['model'] = net_almanac.event 
+        json_event['model'] = 'net_almanac.event' 
         fields = {}
         fields['name'] = event.name
         fields['description'] = event.description
@@ -97,8 +180,8 @@ def serialize_events(events):
         fields['tags'] = event.tags
         json_event['fields'] = fields
         json_container.append(json_event)
-        
-    return 
+    
+    return simplejson.dumps(json_container)
 
 def deserialize_events(json_string):
     """
@@ -120,17 +203,15 @@ def deserialize_events(json_string):
                   fields['tags'])
         to_return.append(e)
     return to_return
-                      
     
-def get_all_events(url):
-    """
-    given the root URL of a net_almanac server, such as 
+        
+def is_empty_or_space(input_string):
+    return (input_string == None or input_string == '' or input_string.isspace())
+
+def is_alnum_or_space(input_string):
+    for char in input_string:
+        if (not char.isalnum() and not char.isspace()):
+            return False
+    return True
     
-    http://www.example.com/net_almanac/event/
-    
-    returns a list of Events
-    """
-    http = httplib2.Http()
-    response, content = http.request(url, 'GET', headers = JSON_HEADERS)
-    
-    return deserialize_events(content)
+               
